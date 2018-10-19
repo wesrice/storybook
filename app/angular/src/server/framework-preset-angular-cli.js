fffead59cae5e0fb318ece37a34354b62d50199a
igor-dv <davydkin.igor@gmail.com>
2018-10-19 10:45:23 +0300

    Remove css rules from default webpack rules, when angular-cli is installed

--- app/angular/src/server/framework-preset-angular-cli.js
+++ app/angular/src/server/framework-preset-angular-cli.js
@@ -1,17 +1,35 @@
 import { logger } from '@storybook/node-logger';
+import { filterOutStylingRules } from './angular-cli_utils';
 
 import {
   getAngularCliWebpackConfigOptions,
   applyAngularCliWebpackConfig,
 } from './angular-cli_config';
 
-export function webpack(config) {
-  const cwd = process.cwd();
-  const cliWebpackConfigOptions = getAngularCliWebpackConfigOptions(cwd);
+const cwd = process.cwd();
+const cliWebpackConfigOptions = getAngularCliWebpackConfigOptions(cwd);
 
-  if (cliWebpackConfigOptions) {
-    logger.info('=> Loading angular-cli config.');
+export function webpack(config) {
+  if (!cliWebpackConfigOptions) {
+    return config;
   }
 
+  logger.info('=> Loading angular-cli config.');
   return applyAngularCliWebpackConfig(config, cliWebpackConfigOptions);
+}
+
+export function webpackDefault(config) {
+  if (!cliWebpackConfigOptions) {
+    return config;
+  }
+
+  const defaultRulesWithoutCss = filterOutStylingRules(config);
+
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: defaultRulesWithoutCss,
+    },
+  };
 }
